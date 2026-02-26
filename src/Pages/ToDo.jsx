9ca3006aa9e611f659ec93b7f94a40ad5e0a8fc0
vishvaiih <vishva.iihglobal@gmail.com>
2026-02-26 10:07:23 +0530

    some changes

--- src/Pages/ToDo.jsx
+++ src/Pages/ToDo.jsx
@@ -1,52 +1,106 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function ToDo() {
+  const [task, setTask] = useState("");
+  const [toDo, setToDo] = useState([]);
 
-  const [task,settask] = useState("");
-  const [toDo,setToDo] = useState([]);
+  const [editId, setEditId] = useState("");
 
   const handleAdd = () => {
-        
+    if (task.trim() === "") {
+      return;
+    }
 
-         if(task.trim() === ""){
-          return;
-         }
+    console.log("task", task);
 
-         console.log("task",task)
+    const newtask = {
+      id: Math.random() * Math.pow(5, 9),
+      task: task,
+    };
 
-         const newtask = {
-          id : Math.random() * Math.pow(5,9),
-          task : task,
-         }
+    setToDo([...toDo, newtask]);
 
-         setToDo([...toDo,newtask])
+    setTask("");
+  };
+
+  useEffect(() => {
+    if (toDo?.length > 0) {
+      localStorage.setItem("task", JSON.stringify(toDo));
+    }
+  }, [task]);
+
+  useEffect(() => {
+    const getTask = JSON.parse(localStorage.getItem("task")) || [];
+    setToDo(getTask);
+  }, []);
+
+  const handleDelete = (id) => {
+    const updatedData = toDo?.filter((itm) => itm.id !== id);
+    setToDo(updatedData);
+    localStorage.setItem("task", JSON.stringify(updatedData));
+  };
+
+  const handleEdit = (id) => {
+    setEditId(id);
+    const find = toDo?.find((itm) => itm.id === id);
+    setTask(find.task);
+  };
+
+  const handleCancel = () => {
+    setTask("");
+    setEditId("");
   }
 
-  console.log("todo",toDo)
+  const handleUpdate = () => {
+    const updateData = toDo?.map((itm) => 
+        itm.id === editId ? {...itm, task: task} : itm
+    
+    )
+
+    setToDo(updateData);
+    setTask("");
+    setEditId("");
+  }
 
   return (
-    <div className='mainDiv'>
-    <p className="todolist">TODO LIST</p>
+    <div className="mainDiv">
+      <p className="todolist">TODO LIST</p>
 
-    <input type='text' className='text' placeholder='Enter Task...' onChange={(e) => settask(e.target.value)}></input>
-    <button className='btn' onClick = {() => handleAdd()}>ADD</button>
+      <input
+        type="text"
+        className="text"
+        placeholder="Enter Task..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      ></input>
+      {!editId ? (
+        <button className="btn" onClick={() => handleAdd()}>
+          ADD
+        </button>
+      ) : (
+        <>
+          <button className="btn" onClick={() => handleCancel()}>
+            CANCEL
+          </button>
+          <button className="btn" onClick={() => handleUpdate()}>
+            UPDATE
+          </button>
+        </>
+      )}
 
-    <div className='tasklist'>
-
-        <div className='sidebyside'>
-          <p>task 1</p>
-          <div className='buttons'>
-            <button>DELETE</button>
-            <button>EDIT</button>
+      <div className="tasklist">
+        {toDo?.map((itm) => (
+          <div className="sidebyside">
+            <p>{itm.task}</p>
+            <div className="buttons">
+              <button onClick={() => handleDelete(itm.id)}>DELETE</button>
+              <button onClick={() => handleEdit(itm.id)}>EDIT</button>
+            </div>
           </div>
-        </div>
-
-        
+        ))}
+      </div>
     </div>
-
-
-  </div> 
-  )
+  );
 }
 
-export default ToDo
+export default ToDo;
